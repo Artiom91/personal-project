78f259779236552d0cc4855bc41cbbb62c1ef93f
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-06-09 09:38:47 +0300

    vite config redact

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Установите желаемый базовый путь здесь
+  base: '/tweak/', // Установите желаемый базовый путь здесь
   plugins: [react()],
   resolve: {
     alias: {
