f04a28c59bdd2de1c3fcb797145dd3a456edd72d
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-06-09 11:48:29 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personal-project/', // Установите желаемый базовый путь здесь
   plugins: [react()],
   resolve: {
     alias: {
